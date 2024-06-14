1ce344d2a628eb1fa440d0ba6d85cb1a5388d3f3
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-06-14 18:25:32 +0300

    Initial project

---
+++ src/main/java/org/example/edutrackerteach/EduTrackerTeachApplication.java
@@ -0,0 +1,13 @@
+package org.example.edutrackerteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrackerTeachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduTrackerTeachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/edutrackerteach/EduTrackerTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.edutrackerteach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackerTeachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
