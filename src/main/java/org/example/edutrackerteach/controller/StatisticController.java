e0202ff6a5b075a519a995d456f83e15e0e765d2
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-06-19 15:30:42 +0300

    Finish sidebar and add spring-boot-devtools to maven.xml

--- src/main/java/org/example/edutrackerteach/controller/StatisticController.java
+++ src/main/java/org/example/edutrackerteach/controller/StatisticController.java
@@ -2,14 +2,20 @@ package org.example.edutrackerteach.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
 public class StatisticController {
-    @GetMapping("/")
+    @GetMapping
     public ModelAndView index(){
         return new ModelAndView("statistic/index");
+    }
+    @ModelAttribute
+    public void activeMenuItem(Model model) {
+        model.addAttribute("statisticActive", true);
     }
 }
